62050e463bd884c188e5a806bae5a48a0cd9b6d5
Spomky <florent+git@morselli.fr>
2017-10-02 00:25:17 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Component/Server/GrantType/RefreshTokenGrantType.php
+++ src/Component/Server/GrantType/RefreshTokenGrantType.php
@@ -98,10 +98,10 @@ final class RefreshTokenGrantType implements GrantTypeInterface
 
         $grantTypeData = $grantTypeData->withResourceOwnerId($token->getResourceOwnerId());
         $grantTypeData = $grantTypeData->withRefreshToken();
-        foreach($token->getMetadatas()->all() as $k => $v) {
+        foreach ($token->getMetadatas()->all() as $k => $v) {
             $grantTypeData = $grantTypeData->withMetadata($k, $v);
         }
-        foreach($token->getParameters()->all() as $k => $v) {
+        foreach ($token->getParameters()->all() as $k => $v) {
             $grantTypeData = $grantTypeData->withParameter($k, $v);
         }
 
