7370de68b5b2cc5ea7a4eb63cccf38e62bb1d05e
Spomky <Spomky@users.noreply.github.com>
2018-01-05 14:55:50 +0100

    Apply fixes from StyleCI (#67)
    [ci skip] [skip ci]

--- src/Component/Server/Middleware/InitialAccessTokenMiddleware.php
+++ src/Component/Server/Middleware/InitialAccessTokenMiddleware.php
@@ -5,14 +5,13 @@ declare(strict_types=1);
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2014-2017 Spomky-Labs
+ * Copyright (c) 2014-2018 Spomky-Labs
  *
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
  */
 
 namespace OAuth2Framework\Component\Server\Middleware;
-
 
 use Interop\Http\Server\RequestHandlerInterface;
 use Interop\Http\Server\MiddlewareInterface;
