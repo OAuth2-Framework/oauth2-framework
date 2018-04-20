efbf2518b3ed690a2f60b8098bc300cf86d5fcc0
Spomky <florent@morselli.fr>
2018-04-20 12:52:18 +0200

    Bug fixed

--- src/ServerBundle/Component/Endpoint/TokenIntrospection/TokenIntrospectionEndpointSource.php
+++ src/ServerBundle/Component/Endpoint/TokenIntrospection/TokenIntrospectionEndpointSource.php
@@ -57,7 +57,7 @@ class TokenIntrospectionEndpointSource implements Component
     {
         $rootNode->validate()
             ->ifTrue(function ($config) {
-                return true === $config['endpoint'][$this->name()]['enabled'] && null === $config['resource_server']['repository'];
+                return true === $config['endpoint'][$this->name()]['enabled'] && (!isset($config['resource_server']) || !isset($config['resource_server']['repository']) || null === $config['resource_server']['repository']);
             })
             ->thenInvalid('The resource server repository must be set when the introspection endpoint is enabled')
         ->end();
