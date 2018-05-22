bdfa913f11a51089171b2fedd4528ceb45e42868
Spomky <florent@morselli.fr>
2018-05-22 19:54:09 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/ServerBundle/Form/Type/AuthorizationType.php
+++ src/ServerBundle/Form/Type/AuthorizationType.php
@@ -18,7 +18,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Translation\TranslatorInterface;
 
 class AuthorizationType extends AbstractType
 {
